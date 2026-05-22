5ea481486c175ec710f4e1c05909414d1fcbe81c
lucaspereirasouza <lucaspereiradesouza2163@outlook.com>
2026-05-22 16:01:51 -0300

    refactor: simplify network.sh by removing legacy code and moving secondary interface configuration to a new file

--- src/entry.sh
+++ src/entry.sh
@@ -17,7 +17,7 @@ cd /run
 . install.sh    # Run installation
 . disk.sh       # Initialize disks
 . display.sh    # Initialize graphics
-. custom_network.sh # Configure second network interface
+# . custom_network.sh # Configure second network interface
 . network.sh    # Initialize network
 . samba.sh      # Configure samba
 . boot.sh       # Configure boot
